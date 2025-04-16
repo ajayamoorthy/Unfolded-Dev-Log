edb8ccff5d17fb77d201a40a6d5505319bb24c39
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2025-04-15 22:56:41 -0400

    added zip file download to alpha page attempt 3

--- src/pages/Alpha.jsx
+++ src/pages/Alpha.jsx
@@ -1,13 +1,44 @@
+import UnfoldedAlpha from "../assets/files/UnfoldedAlpha.zip";
+import ButtonGradient from "../assets/svg/ButtonGradient";
+import Button from "../components/Button";
+
 const Alpha = () => {
   return (
-    <div className="p-10 text-center">
-      <h1 className="text-4xl font-bold">Alpha Page</h1>
-      <p>Under Construction!</p>
-      <img
-        src="./imgs/warningsign.png"
-        alt="Warning Sign"
-        className="mx-auto w-1/3 max-w-[200px] h-auto"
-      ></img>
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-center mb-10">Alpha Release</h1>
+
+      <div className="flex flex-col lg:flex-row items-center lg:items-start gap-10">
+        <div className="w-full lg:w-1/2">
+          {/* <video
+            src="/demo.mp4"
+            controls
+            className="w-full rounded-xl shadow-lg"
+          /> */}
+          <img
+            src="./imgs/kylie/gaga_turnaround_render.jpg"
+            alt="Alpha Preview"
+            className="w-full rounded-xl shadow-lg"
+          />
+        </div>
+
+        <div className="w-full lg:w-1/2 text-left">
+          <h2 className="text-2xl font-bold mb-4">Build State</h2>
+          <p className="body-1 mb-6 text-n-2">
+            The player can play as Colin, the magical boy. The enemy models have
+            been implemented into the project, along with the work in progress
+            environment. Player can teleport between the different locations in
+            the graybox and attack the enemies and kill them.
+          </p>
+
+          <Button className="text-2xl">
+            <a href={UnfoldedAlpha} target="_blank" rel="noopener noreferrer">
+              Alpha Download
+            </a>
+          </Button>
+        </div>
+      </div>
+
+      <ButtonGradient />
     </div>
   );
 };
