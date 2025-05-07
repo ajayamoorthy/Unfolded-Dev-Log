3d6d2a29e6115bf8dc91166eb28395d4a19096ea
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2025-05-07 16:04:21 -0400

    attempt to fix pdfs 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import AnuPortfolio from "../assets/files/AnanyaJayamoorthyPortfolio.pdf";
-import LadaPortfolio from "../assets/files/Lada_Portfolio.pdf";
+import LadaPortfolio from "../assets/files/Lada_PortFolio.pdf";
+import ButtonGradient from "../assets/svg/ButtonGradient";
+import Button from "../components/Button";
 
 const Home = () => {
   return (
-    <div className="p-10 text-center">
+    <div className="p-10 text-center m-2">
       <h1 className="text-4xl font-bold">Home Page</h1>
       <p>Welcome to the homepage!</p>
       <Button className="mt-10 text-2xl">
@@ -11,6 +13,7 @@ const Home = () => {
           Anu's Tech Portfolio
         </a>
       </Button>
+
       <Button className="mt-10 text-2xl">
         <a href={LadaPortfolio} target="_blank" rel="noopener noreferrer">
           Lada's Tech Portfolio
